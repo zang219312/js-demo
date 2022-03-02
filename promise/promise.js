726013c92a15756982bef3a3c3feb0c7bf391736
zhangding <2324674973@qq.com>
2022-03-02 23:26:45 +0800

    Class 版本封装

--- promise/promise.js
+++ promise/promise.js
@@ -1,161 +1,164 @@
-function MyPromise(executor) {
-  this.PromiseState = 'pending'
-  this.PromiseResult = null
-  // 声明一个属性
-  this.callbacks = []
+class MyPromise {
+  constructor(executor) {
+    this.PromiseState = 'pending'
+    this.PromiseResult = null
+    // 声明一个属性
+    this.callbacks = []
 
-  // resolve 函数
-  function resolve(data) {
-    if (this.PromiseState !== 'pending') return
-    // ? 1.修改对象的状态 （promiseState）
-    this.PromiseState = 'fulfilled' // resolved
-    // ? 2.设置对象结果值 （promiseResult）
-    this.PromiseResult = data
-    // 调用成功的回调函数
-    setTimeout(() => {
-      this.callbacks.forEach(item => {
-        item.onResolved(data)
+    // resolve 函数
+    function resolve(data) {
+      if (this.PromiseState !== 'pending') return
+      // ? 1.修改对象的状态 （promiseState）
+      this.PromiseState = 'fulfilled' // resolved
+      // ? 2.设置对象结果值 （promiseResult）
+      this.PromiseResult = data
+      // 调用成功的回调函数
+      setTimeout(() => {
+        this.callbacks.forEach(item => {
+          item.onResolved(data)
+        })
       })
-    })
-  }
+    }
 
-  // reject
-  function reject(data) {
-    if (this.PromiseState !== 'pending') return
-    this.PromiseState = 'rejected'
-    this.PromiseResult = data
-    // 调用失败的回调函数
+    // reject
+    function reject(data) {
+      if (this.PromiseState !== 'pending') return
+      this.PromiseState = 'rejected'
+      this.PromiseResult = data
+      // 调用失败的回调函数
 
-    setTimeout(() => {
-      this.callbacks.forEach(item => {
-        item.onRejected()
+      setTimeout(() => {
+        this.callbacks.forEach(item => {
+          item.onRejected()
+        })
       })
-    })
-  }
+    }
 
-  try {
-    // 同步调用 【执行器函数】
-    executor(resolve.bind(this), reject.bind(this))
-  } catch (error) {
-    // 修改 promise的状态为 【失败】
-    reject.call(this, error)
-  }
-}
-
-// add function myThen
-MyPromise.prototype.myThen = function (onResolved, onRejected) {
-  const self = this
-  // 判断回调函数参数
-  if (typeof onRejected !== 'function') {
-    onRejected = reason => {
-      throw reason
+    try {
+      // 同步调用 【执行器函数】
+      executor(resolve.bind(this), reject.bind(this))
+    } catch (error) {
+      // 修改 promise的状态为 【失败】
+      reject.call(this, error)
     }
   }
 
-  if (typeof onResolved !== 'function') {
-    onResolved = res => res
-  }
-  return new MyPromise((resolve, reject) => {
-    function callback(type) {
-      try {
-        const result = type(self.PromiseResult)
-        // 如果是 MyPromise 实例
-        if (result instanceof MyPromise) {
-          result.myThen(
-            v => resolve(v),
-            r => reject(r)
-          )
-        } else {
-          resolve(result)
-        }
-      } catch (e) {
-        reject(e)
+  // then
+  myThen(onResolved, onRejected) {
+    const self = this
+    // 判断回调函数参数
+    if (typeof onRejected !== 'function') {
+      onRejected = reason => {
+        throw reason
       }
     }
 
-    if (this.PromiseState === 'fulfilled') {
-      // 回调的执行结果
-      setTimeout(() => {
-        callback(onResolved)
-      })
+    if (typeof onResolved !== 'function') {
+      onResolved = res => res
     }
-
-    if (this.PromiseState === 'rejected') {
-      setTimeout(() => {
-        callback(onRejected)
-      })
-    }
-
-    // 判断pending 状态
-    if (this.PromiseState === 'pending') {
-      // 保存回调函数
-      this.callbacks.push({
-        onResolved() {
-          callback(onResolved)
-        },
-        onRejected() {
-          callback(onRejected)
-        }
-      })
-    }
-  })
-}
-
-MyPromise.prototype.myCatch = function (onRejected) {
-  return this.myThen(undefined, onRejected)
-}
-
-MyPromise.resolve = function (val) {
-  return new MyPromise((resolve, reject) => {
-    if (val instanceof MyPromise) {
-      val.myThen(
-        v => resolve(v),
-        r => reject(r)
-      )
-    } else {
-      resolve(val)
-    }
-  })
-}
-
-MyPromise.reject = function (reason) {
-  return new MyPromise((resolve, reject) => {
-    reject(reason)
-  })
-}
-
-MyPromise.all = function (promises) {
-  return new MyPromise((resolve, reject) => {
-    let count = 0
-    let arr = []
-    for (let i = 0; i < promises.length; i++) {
-      promises[i].myThen(
-        v => {
-          // 对象的状态是成功
-          // 每个promise对象的状态 都成功
-          count += 1
-          arr[i] = v
-
-          if (count === promises.length) {
-            resolve(arr)
+    return new MyPromise((resolve, reject) => {
+      function callback(type) {
+        try {
+          const result = type(self.PromiseResult)
+          // 如果是 MyPromise 实例
+          if (result instanceof MyPromise) {
+            result.myThen(
+              v => resolve(v),
+              r => reject(r)
+            )
+          } else {
+            resolve(result)
           }
-        },
-        r => reject(r)
-      )
-    }
-  })
-}
+        } catch (e) {
+          reject(e)
+        }
+      }
 
-MyPromise.race = function (promises) {
-  return new MyPromise((resolve, reject) => {
-    for (const item of promises) {
-      item.myThen(
-        v => {
-          // 修改返回promise对象的状态时成功
-          resolve(v)
-        },
-        r => reject(r)
-      )
-    }
-  })
+      if (this.PromiseState === 'fulfilled') {
+        // 回调的执行结果
+        setTimeout(() => {
+          callback(onResolved)
+        })
+      }
+
+      if (this.PromiseState === 'rejected') {
+        setTimeout(() => {
+          callback(onRejected)
+        })
+      }
+
+      // 判断pending 状态
+      if (this.PromiseState === 'pending') {
+        // 保存回调函数
+        this.callbacks.push({
+          onResolved() {
+            callback(onResolved)
+          },
+          onRejected() {
+            callback(onRejected)
+          }
+        })
+      }
+    })
+  }
+
+  // catch
+  myCatch(onRejected) {
+    return this.myThen(undefined, onRejected)
+  }
+
+  static resolve(val) {
+    return new MyPromise((resolve, reject) => {
+      if (val instanceof MyPromise) {
+        val.myThen(
+          v => resolve(v),
+          r => reject(r)
+        )
+      } else {
+        resolve(val)
+      }
+    })
+  }
+
+  static reject(reason) {
+    return new MyPromise((resolve, reject) => {
+      reject(reason)
+    })
+  }
+
+  static all(promises) {
+    return new MyPromise((resolve, reject) => {
+      let count = 0
+      let arr = []
+      for (let i = 0; i < promises.length; i++) {
+        promises[i].myThen(
+          v => {
+            // 对象的状态是成功
+            // 每个promise对象的状态 都成功
+            count += 1
+            arr[i] = v
+
+            if (count === promises.length) {
+              resolve(arr)
+            }
+          },
+          r => reject(r)
+        )
+      }
+    })
+  }
+
+  static race(promises) {
+    return new MyPromise((resolve, reject) => {
+      for (const item of promises) {
+        item.myThen(
+          v => {
+            // 修改返回promise对象的状态时成功
+            resolve(v)
+          },
+          r => reject(r)
+        )
+      }
+    })
+  }
 }
